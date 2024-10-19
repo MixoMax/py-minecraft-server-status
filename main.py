7e92b48de233fde54f314089957fa4246198cac0
Linus Horn <linus@linushorn.dev>
2024-10-19 14:56:38 +0200

    working logger

--- main.py
+++ main.py
@@ -11,22 +11,25 @@ host = "fsr-informatik.de"
 db = sqlite3.connect("stats.db")
 cursor = db.cursor()
 
-cmd = "CREATE TABLE IF NOT EXISTS stats (time INTEGER, num_players INTEGER, max_players INTEGER)"
+cmd = """CREATE TABLE IF NOT EXISTS stats 
+(time INTEGER PRIMARY KEY,
+num_players INTEGER,
+max_players INTEGER,
+players TEXT)"""
 cursor.execute(cmd)
 db.commit()
 
 
 
-def get_stats(host: str) -> tuple[int, int]:
+def get_stats(host: str) -> tuple[int, int, list[str]]:
     res = requests.get(f"https://mcsrvstat.us/server/{host}")
 
     if res.status_code != 200:
         print(f"Error: {res.status_code}")
-        return -res.status_code, -res.status_code
+        return -res.status_code, -res.status_code, []
     
-
-
     html = res.text
+    og_html = html
 
     # search for all <div class="col-md-10 mb-3">
     q_results = []
@@ -44,10 +47,38 @@ def get_stats(host: str) -> tuple[int, int]:
         match = regex.search(q)
         if match:
             num_players = int(match.group(1))
-            print(match.group(1))
             max_players = int(match.group(2))
 
-    return num_players, max_players
+    
+    players = []
+    q_str = '<a href="https://namemc.com/profile/'
+    end_str = "</a>"
+
+    html = og_html
+
+    while q_str in html:
+        html = html[html.find(q_str) + len(q_str):]
+
+        uuid_end = '"'
+        uuid = html[:html.find(uuid_end)]
+        html = html[html.find(uuid_end):]
+
+        name_start = 'title="'
+        name_end = '" />'
+
+        name = html[html.find(name_start) + len(name_start):html.find(name_end)]
+        html = html[html.find(name_end):]
+
+        players.append(name)
+    
+    return num_players, max_players, players
+
+
+
+
+    
+
+
 
 
 print("Linus Horn´s Minecraft Server Status logger")
@@ -62,7 +93,10 @@ while True:
     current_time = int(time.time())
     if current_time - last_query >= interval:
         last_query = current_time
-        num_players, max_players = get_stats(host)
-        print(f"Num players: {num_players}, Max players: {max_players}")
-        cursor.execute("INSERT INTO stats (time, num_players, max_players) VALUES (?, ?, ?)", (current_time, num_players, max_players))
+        num_players, max_players, players = get_stats(host)
+        
+        players_str = ",".join(players)
+
+        print(f"{current_time}: {num_players}/{max_players} players: {players_str}")
+        cursor.execute("INSERT INTO stats VALUES (?, ?, ?, ?)", (current_time, num_players, max_players, players_str))
         db.commit()
