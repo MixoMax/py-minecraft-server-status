d6ab2d71046859ceb139906818ddbe4c280b24f1
Linus Horn <linus@linushorn.dev>
2024-11-06 13:52:51 +0100

    Update server.py

--- server.py
+++ server.py
@@ -82,4 +82,4 @@ async def get_stats(t_start: str, t_end: str):
     
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8002)
+    uvicorn.run(app, host="0.0.0.0", port=8002)
